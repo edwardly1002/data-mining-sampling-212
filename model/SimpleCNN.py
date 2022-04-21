03efc204a8139c239e870b6cd8e2ab720bb031c7
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2022-04-21 21:59:04 +0700

    SimpleCNN to test flow

---
+++ model/SimpleCNN.py
@@ -0,0 +1,10 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+class SimpleCNN(keras.Model):
+    def __init__(self):
+        super.__init__()
+        self.CNNs = [
+            
+        ]
+        self.Flatten = layers.Flatten()
