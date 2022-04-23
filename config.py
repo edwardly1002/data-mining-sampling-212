36107585a829c5c1245f401b11b678ab675322c8
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2022-04-23 21:52:35 +0700

    train process

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 EPOCH = 1000
+BATCH_SIZE = 32
+STEPS_PER_EPOCH = None
 DATASET_URL = "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
--- train.py
+++ train.py
@@ -1,2 +1,52 @@
 from dataset import *
-from model.Model import Model
+from model.SimpleCNN import SimpleCNN
+
+import tensorflow as tf
+from config import *
+
+def main():
+    ### STEP 1: PREPARE DATASET
+    train_dataset = (
+        tf.data.Dataset.from_generator(
+            ### generator, 
+            # output_signature = (
+            #     tf.TensorSpec(shape=(), dtype=tf.float32), 
+            #     tf.TensorSpec(shape=(), dtype=tf.float32,)  
+            # )
+        )
+        .batch(BATCH_SIZE, drop_remainder=False)
+        # .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+
+    ### STEP 2: Build model
+    model = SimpleCNN()
+    model.build(input_shape = ())
+    model.summary()
+    model.compile(
+        # optimizer=,
+        # loss = BinaryCrossentropy(), 
+        # metrics = ['accuracy', precision_m, recall_m, f1_m]
+    )
+    
+    model_checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+        # ckpt_dir,
+        # monitor='val_f1',
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=False,
+        mode='max',
+        save_freq='epoch',
+    )
+    
+    ### STEP 3: TRAIN
+    history = model.fit(
+        train_dataset,
+        # validation_data=test_dataset,
+        steps_per_epoch=STEPS_PER_EPOCH,
+        epochs=EPOCH,
+        verbose=1,
+        callbacks=[model_checkpoint_callback]
+    )
+
+if __name__ == "__main__":
+    main()
