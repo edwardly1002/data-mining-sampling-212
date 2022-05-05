820ce024a56bf1cb9bd2e5fd092d1184e0a70cc0
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2022-05-05 14:48:12 +0700

    fix logging to file

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ DATASET_URL = "https://storage.googleapis.com/download.tensorflow.org/data/credi
 DATASET_DIR = "data/creditcard.csv"
 
 DEBUG = True
-LOG_DIR = None
-def log(s):
+def log(s, log_dir=None):
     from datetime import datetime
-    if DEBUG and not LOG_DIR: print(f'[DEBUG] {str(datetime.now())}:\t{str(s)}')
-    elif DEBUG and LOG_DIR: 
-        with open(LOG_DIR, 'a') as f:
+    if DEBUG and not log_dir: print(f'[DEBUG] {str(datetime.now())}:\t{str(s)}')
+    elif DEBUG and log_dir: 
+        with open(log_dir, 'a') as f:
             f.write(f'[DEBUG] {str(datetime.now())}:\t{str(s)}\n')
