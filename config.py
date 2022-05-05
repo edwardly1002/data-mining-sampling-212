473570bc0e5fa278d50bf9e7570f8954079dbc9c
Lý Thanh Bách <40271986+edwardly1002@users.noreply.github.com>
2022-05-05 14:26:30 +0700

    Add dataset directory and DEBUG logging

--- config.py
+++ config.py
@@ -2,3 +2,9 @@ EPOCH = 1000
 BATCH_SIZE = 32
 STEPS_PER_EPOCH = None
 DATASET_URL = "https://storage.googleapis.com/download.tensorflow.org/data/creditcard.csv"
+DATASET_DIR = "data/creditcard.csv"
+
+DEBUG = True
+def log(s):
+    from datetime import datetime
+    if DEBUG: print(f'[DEBUG] {str(datetime.now())}:\t{s}')
